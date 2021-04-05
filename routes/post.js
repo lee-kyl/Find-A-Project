be71fb1b807bcf65ec386fd868b225b4eb72d875
fischkoder <63186539+fischkoder@users.noreply.github.com>
2021-04-05 23:08:30 +0800

    mongoose api tested

--- routes/post.js
+++ routes/post.js
@@ -6,11 +6,30 @@ router.get('/showUsers',
     (req, res) => {
         user.find()
         .then(result => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch(err => {
             console.log(err);
         })
     });
+
+router.post('/saveSampleUser', (req, res) => {
+    newUser = new user ({
+        userNumber: '33333333',
+        userType: 'Student',
+        firstName: 'Kun',
+        familyName: 'young',
+        email: 'a@d.com',
+        potrait: null
+    }),
+    newUser.save()
+        .then(() => {
+            res.status(200).send('User Created');
+        })
+        .catch(err => {
+            res.status(500).send('Unable to create');
+            console.log(err);
+        })
+});
 
 module.exports = router;
