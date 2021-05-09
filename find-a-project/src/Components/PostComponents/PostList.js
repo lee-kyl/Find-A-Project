93b456fb870ac35fdad519ad528e22813ae8f8d2
4kyl_aws <lee.li1993.aws@gmail.com>
2021-05-09 11:18:19 +1000

    Init basic setup

--- find-a-project/src/Components/PostComponents/PostList.js
+++ find-a-project/src/Components/PostComponents/PostList.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from './Post'
 
 export default function PostList() {
-    return (
-        <div>
-            <p></p>
-            <Post />
+    const fetchData = async (url) => {
+        // const response = await fetch(url);
+        // let data = await response.json();
+        // return data;
+        // TODO: replace mock function
+        
+        return mockAPICall();
+    }
 
-        </div>
+    const [postList, setPostList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        getData();
+    })
+
+    // Mock API call
+    const mockAPICall = () => new Promise(resolve => setTimeout(resolve, 2000));
+
+    const getData = () => {
+        return fetchData('').then((result) => {
+            setPostList([{id: 1}, {id: 2}])
+            setIsLoading(false)
+        }).catch((err) => {
+            setPostList([])
+            setIsLoading(true)
+        });
+    }
+
+    const renderLoadingSpinner = () => <p>is loading ...</p>
+
+    const renderPost = () => {
+        const posts = postList?.map(post => {
+            return <Post></Post>
+        });
+
+        return <>{posts}</>
+    }
+
+    return (
+        <>
+            {isLoading ? renderLoadingSpinner() : renderPost()}
+        </>
     )
 }
