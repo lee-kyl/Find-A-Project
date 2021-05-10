7ceb67d77ec0dd7efb4f01a63653d78f8ac088d6
4kyl_aws <lee.li1993.aws@gmail.com>
2021-05-11 00:23:11 +1000

    Update UI for Discussion Card, Project Card, TeamUp Card

--- find-a-project/src/Components/PostComponents/ProjectCard.js
+++ find-a-project/src/Components/PostComponents/ProjectCard.js
@@ -1,51 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import PostCard from './PostCard';
+import { Chip } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    maxHeight: 300
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-export default function ProjectCard() {
-  const classes = useStyles();
-
+export default function ProjectCard(props) {
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          The Information Block
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Project Name: Social Digital Platform
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Area: Information Technology
-        </Typography>
-        <Typography variant="body2" component="p">
-
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" href="/post/1">Learn More</Button>
-      </CardActions>
-    </Card>
+    <PostCard post={props.post}>
+        <Chip variant="outlined" label={`Slot: ${props.post?.addition?.slot}`} />
+        <Chip variant="outlined" label={`Faculty: ${props.post?.addition?.school}`} />
+        <Chip variant="outlined" label={`Area: ${props.post?.addition?.major}`} />
+    </PostCard>
   );
 }
--- find-a-project/src/Components/PostComponents/TeamUpCard.js
+++ find-a-project/src/Components/PostComponents/TeamUpCard.js
@@ -1,51 +1,11 @@
+import { Chip } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import PostCard from './PostCard';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    maxHeight: 300
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-export default function TeamUpCard() {
-  const classes = useStyles();
-
+export default function TeamUpCard(props) {
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          The Information Block
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Project Name: Social Digital Platform
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Area: Information Technology
-        </Typography>
-        <Typography variant="body2" component="p">
-
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" href="/post/1">Learn More</Button>
-      </CardActions>
-    </Card>
+    <PostCard post={props.post}>
+      <Chip variant="outlined" label={`Team: ${props.post?.addition?.team}`} />
+    </PostCard>
   );
 }
